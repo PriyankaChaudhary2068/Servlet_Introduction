48f07017eae62789721416c12201e1e7f9915cff
chutki12 <priyankachaudhary2068>
2022-08-24 18:47:26 +0530

    ADDED : Welcome To Servlet introduction

---
+++ src/main/java/com/bridgelabz/servletintroduction/SimpleServlet.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.servletintroduction;
+
+public class SimpleServlet {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Servlet Introduction!");
+    }
+}
+
+
+
+
