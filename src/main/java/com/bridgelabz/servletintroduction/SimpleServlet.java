66b80d0a7d9e704609a49e8ec4c6e92abfb3bd30
chutki12 <priyankachaudhary2068>
2022-08-24 21:37:50 +0530

    ADDED : Created First Simple Servlet

--- src/main/java/com/bridgelabz/servletintroduction/SimpleServlet.java
+++ src/main/java/com/bridgelabz/servletintroduction/SimpleServlet.java
@@ -1,10 +1,23 @@
 package com.bridgelabz.servletintroduction;
 
-public class SimpleServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-    public static void main(String[] args) {
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3> Hello Priyanka My First Servlet! </h3>");
+        out.close();
 
-        System.out.println("Welcome To Servlet Introduction!");
+
     }
 }
 
